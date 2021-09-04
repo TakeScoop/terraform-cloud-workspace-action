d5ec63357390f28c9fb20923f74d6073efa8d7ef
Ryan Wholey <ryan@takescoop.com>
2021-09-03 21:49:12 -0600

    create workspace

--- internal/action/main_test.go
+++ internal/action/main_test.go
@@ -6,10 +6,43 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/go-tfe"
 	"github.com/stretchr/testify/assert"
 )
+
+func RemoveTestWorkspaces(ctx context.Context, client *tfe.Client, organization string, prefix string) error {
+	workspaces, err := client.Workspaces.List(ctx, organization, tfe.WorkspaceListOptions{
+		Search: &prefix,
+		ListOptions: tfe.ListOptions{
+			PageSize: 100,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	for _, ws := range workspaces.Items {
+		if err := client.Workspaces.DeleteByID(ctx, ws.ID); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func SetTestEnvs(envs map[string]string) {
+	for key, value := range envs {
+		os.Setenv(fmt.Sprintf("INPUT_%s", strings.ToUpper(key)), value)
+	}
+}
+
+func UnsetTestEnvs(envs map[string]string) {
+	for key := range envs {
+		os.Unsetenv(fmt.Sprintf("INPUT_%s", strings.ToUpper(key)))
+	}
+}
 
 func TestCreateWorkspace(t *testing.T) {
 	if testing.Short() {
@@ -22,7 +55,7 @@ func TestCreateWorkspace(t *testing.T) {
 		"terraform_token":          os.Getenv("tf_token"),
 		"terraform_organization":   "ryanwholey",
 		"terraform_host":           "app.terraform.io",
-		"name":                     "action-test",
+		"name":                     fmt.Sprintf("action-test-%d", time.Now().Unix()),
 		"import":                   "true",
 		"apply":                    "true",
 		"tfe_provider_version":     "0.25.3",
@@ -30,15 +63,17 @@ func TestCreateWorkspace(t *testing.T) {
 		"terraform_version":        "1.0.5",
 	}
 
-	for key, value := range envs {
-		os.Setenv(fmt.Sprintf("INPUT_%s", strings.ToUpper(key)), value)
-	}
+	SetTestEnvs(envs)
 
 	client, err := tfe.NewClient(&tfe.Config{
 		Address: fmt.Sprintf("https://%s", envs["terraform_host"]),
 		Token:   envs["terraform_token"],
 	})
 	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := RemoveTestWorkspaces(ctx, client, envs["terraform_organization"], "action-test"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -60,13 +95,56 @@ func TestCreateWorkspace(t *testing.T) {
 	assert.Equal(t, ws.Name, envs["name"])
 
 	t.Cleanup(func() {
-		for key := range envs {
-			os.Unsetenv(fmt.Sprintf("INPUT_%s", strings.ToUpper(key)))
-		}
+		UnsetTestEnvs(envs)
 
 		err = client.Workspaces.Delete(ctx, envs["terraform_organization"], envs["name"])
 		if err != nil {
 			t.Fatal(err)
 		}
 	})
+}
+
+func TestImportExistingResources(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+
+	envs := map[string]string{
+		"terraform_token":          os.Getenv("tf_token"),
+		"terraform_organization":   "ryanwholey",
+		"terraform_host":           "app.terraform.io",
+		"name":                     fmt.Sprintf("action-test-%d", time.Now().Unix()),
+		"import":                   "true",
+		"apply":                    "true",
+		"tfe_provider_version":     "0.25.3",
+		"runner_terraform_version": "1.0.5",
+		"terraform_version":        "1.0.5",
+	}
+
+	SetTestEnvs(envs)
+
+	client, err := tfe.NewClient(&tfe.Config{
+		Address: fmt.Sprintf("https://%s", envs["terraform_host"]),
+		Token:   envs["terraform_token"],
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := RemoveTestWorkspaces(ctx, client, envs["terraform_organization"], "action-test"); err != nil {
+		t.Fatal(err)
+	}
+
+	ws, err := client.Workspaces.Create(ctx, envs["terraform_organization"], tfe.WorkspaceCreateOptions{
+		Name:             strPtr(envs["name"]),
+		TerraformVersion: strPtr("1.0.4"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, ws.TerraformVersion, "1.0.4")
+
 }
