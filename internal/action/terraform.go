53b9a592da578a30784506614948cbcea8268252
Ryan Wholey <ryan@takescoop.com>
2021-09-13 12:47:58 -0600

    Dont mutate module

--- internal/action/terraform.go
+++ internal/action/terraform.go
@@ -22,9 +22,10 @@ func NewTerraformExec(ctx context.Context, workDir string, version string) (*tfe
 
 // CopyStateToBackend copies state from the current backend to the passed backend by running Terraform Init
 func CopyStateToBackend(ctx context.Context, tf *tfexec.Terraform, module *tfconfig.Module, backend map[string]interface{}, filePath string) error {
-	module.Terraform.Backend = backend
+	mod := module
+	mod.Terraform.Backend = backend
 
-	if err := TerraformInit(ctx, tf, module, filePath); err != nil {
+	if err := TerraformInit(ctx, tf, mod, filePath); err != nil {
 		return err
 	}
 
