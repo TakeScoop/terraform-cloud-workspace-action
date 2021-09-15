4b3b2a8324d9d66a1ce97d5003976eea334c0001
Ryan Wholey <ryan@takescoop.com>
2021-09-15 11:41:53 -0600

    add back missing multi workspace test

--- internal/action/main_test.go
+++ internal/action/main_test.go
@@ -271,6 +271,29 @@ func TestMultipleWorkspaces(t *testing.T) {
 
 	inputs := newTestInputs(t)
 
+	inputs.Workspaces = `---
+	- staging
+	- production`
+
+	inputs.WorkspaceVariables = `---
+	staging:
+		- key: environment
+			value: staging
+			category: env
+	production:
+		- key: environment
+			value: production
+			category: env`
+
+	inputs.Tags = `---
+	- all`
+
+	inputs.WorkspaceTags = `---
+	staging:
+		- staging
+	production:
+		- production`
+
 	client, err := tfe.NewClient(&tfe.Config{
 		Address: fmt.Sprintf("https://%s", inputs.Host),
 		Token:   inputs.Token,
